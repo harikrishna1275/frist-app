c579160e453d46ba94f103d903817c703e001461
katakam001 <katakam001@gmail.com>
2020-05-13 21:14:30 +0530

    Form Control changes

--- src/app/employee-componet/employee-componet.component.ts
+++ src/app/employee-componet/employee-componet.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-employee-componet',
   templateUrl: './employee-componet.component.html',
-  styleUrls: ['./employee-componet.component.scss']
+  styleUrls: ['./employee-componet.component.scss'],
 })
 export class EmployeeComponetComponent implements OnInit {
+  buttonPressed=false;
+  emailFormControl = new FormControl('', [
+    Validators.required
+  ]);
+  password = new FormControl('', [
+    Validators.required
+  ]);
+  confirmPassword = new FormControl('', [
+    Validators.required
+  ]);
+  matcher = new MyErrorStateMatcher();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  display(){
+  this.buttonPressed=true;
   }
 
+  ngOnInit(): void {}
+}
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
 }
